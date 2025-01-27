988893fe438259a1da88484e214b2ca6c1413a96
andritianaa <andritianasteve@gmail.com>
2025-01-27 15:39:34 +0300

    Implement user authentication redirection in layout components; refactor imports and enhance layout styling

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,7 +1,6 @@
 import { redirect } from 'next/navigation';
 import { ReactNode } from 'react';
 
-import { Sidebar } from '@/components/(main)/navigation/sidebar';
 import { currentUser } from '@/lib/current-user';
 
 export default async function RouteLayout({
@@ -10,12 +9,6 @@ export default async function RouteLayout({
   children: ReactNode
 }) {
   const user = await currentUser()
-  if (!user) return redirect("/auth")
-
-  return (
-    <>
-      <Sidebar />
-      <main className="min-h-screen p-8 pt-4 lg:pl-64">{children}</main>
-    </>
-  )
+  if (user) return redirect("/explore")
+  return <>{children}</>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { redirect } from 'next/navigation';
+
 import { Features } from '@/components/landing/features';
 import { Footer } from '@/components/landing/footer';
 import { LandingGallery } from '@/components/landing/gallery';
@@ -5,8 +7,11 @@ import { Hero } from '@/components/landing/hero';
 import LandingNavbar from '@/components/landing/navbar';
 import { ScrollName } from '@/components/landing/scroll-name';
 import { Testimonials } from '@/components/landing/testimonials';
+import { currentUser } from '@/lib/current-user';
 
 export default async function RoutePage() {
+  const user = await currentUser()
+  if (user) return redirect("/explore")
   return (
     <div>
       <LandingNavbar />

b8ca347a25d411b2d482c3d171a7cfea6d4176d3
andritianaa <andritianasteve@gmail.com>
2025-02-05 15:58:16 +0300

    landing

---
+++ src/components/landing/ui-presentation.tsx
@@ -0,0 +1,11 @@
+export const UiPresentation = () => {
+  return (
+    <div className="shadow-2xl] mt-12 flex w-full items-center justify-center overflow-hidden bg-[#010101]">
+      <img
+        alt=""
+        src={"/assets/UI.png"}
+        className={"h-auto w-[1777px] bg-[#010101]"}
+      />
+    </div>
+  )
+}

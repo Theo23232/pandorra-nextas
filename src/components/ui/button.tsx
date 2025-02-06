d1e992e3c87a6f8bc576688ee3fe5692b4c2b7e7
andritianaa <andritianasteve@gmail.com>
2025-02-06 16:52:48 +0300

    magic button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,7 +13,8 @@ const buttonVariants = cva(
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
         gradient:
           "hover:shadow-gradient mr-2 h-11 border-none bg-gradient-to-l from-[#9600ff] to-[#00ccff] px-6 text-white transition-all ease-in-out hover:scale-[1.01] hover:shadow-[0_4px_15px_0] hover:shadow-[#9600ff]/30",
-
+        magic:
+          "rounded-sm border-none bg-[linear-gradient(325deg,#0044ff_0%,#2ccfff_55%,#0044ff_90%)] bg-[280%_auto] px-6 py-2 font-medium text-white shadow-[0px_0px_20px_rgba(71,184,255,0.5),0px_5px_5px_-1px_rgba(58,125,233,0.25),inset_4px_4px_8px_rgba(175,230,255,0.5),inset_-4px_-4px_8px_rgba(19,95,216,0.35)] transition-[background] duration-700 hover:bg-right-top focus:outline-none focus:ring-blue-400 focus:ring-offset-1 focus:ring-offset-white focus-visible:ring-2 dark:focus:ring-blue-500 dark:focus:ring-offset-black",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
@@ -31,7 +32,7 @@ const buttonVariants = cva(
       },
     },
     defaultVariants: {
-      variant: "gradient",
+      variant: "magic",
       size: "default",
     },
   },

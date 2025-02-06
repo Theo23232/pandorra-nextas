c0871de7db5eb2933d1c1118fe6f6c87559f740b
andritianaa <andritianasteve@gmail.com>
2025-02-06 16:35:51 +0300

    ok

--- src/components/landing/corp.tsx
+++ src/components/landing/corp.tsx
@@ -1,21 +1,22 @@
+import Bounce from "@/components/animated/uibeats/bounce"
+import Marquee from "@/components/nyxb/marquee"
+
 export const Corp = () => {
   return (
-    <div className="max-w-screen relative">
-      <div className="flex items-center justify-center gap-20 py-14">
+    <Bounce className="relative flex size-full flex-col items-center justify-center overflow-hidden rounded-lg border py-20 md:shadow-xl">
+      <Marquee
+        pauseOnHover
+        className="flex items-center gap-10 [--duration:30s]"
+      >
         {Array.from({ length: 9 }, (_, i) => (
           <img
             key={i}
             src={`/assets/corp/${i + 1}.svg`}
-            className="h-12 w-auto"
+            className="ml-40 h-12 w-auto"
             alt={`corp ${i + 1}`}
           />
         ))}
-      </div>
-      <img
-        src="/assets/hallo-1.svg"
-        alt=""
-        className="absolute -top-20 left-0 h-80 w-screen scale-[3]"
-      />
-    </div>
+      </Marquee>
+    </Bounce>
   )
 }

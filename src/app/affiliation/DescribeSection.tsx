fcc4383f26bde1b66b29605b7f77d59435339b1b
Miandry05 <miandryrakotovelo@gmail.com>
2025-02-07 16:24:35 +0300

    affiliate landing done

--- src/app/affiliation/DescribeSection.tsx
+++ src/app/affiliation/DescribeSection.tsx
@@ -1,99 +1,89 @@
+import Bounce from '@/components/animated/uibeats/bounce';
+
 export const DescribeSection = () => {
   return (
-    <div className="mb-8 mt-[600px] flex flex-col items-center justify-center gap-4">
+    <Bounce className="mb-8 mt-[600px] flex flex-col items-center justify-center gap-4">
       <div className="flex w-full flex-col items-center justify-center gap-8">
         <p className="text-[64px] font-semibold leading-[68px] text-white">
-          How it Works
+          How it {""}
+          <span className="bg-gradient-to-r from-[#0099FF] to-[#CC00FF] bg-clip-text text-transparent">
+            Works
+          </span>
         </p>
         <div
-          className="flex w-[789px] items-center justify-start gap-[66px] rounded-[16px] p-[40px] text-white"
+          className="rounded-[16px] bg-gradient-to-r from-[#0099FF] to-[#CC00FF] p-[1.5px]"
           style={{
-            border: "1px solid rgba(255, 255, 255, 0.10)",
-            background: "rgba(116, 114, 138, 0.20)",
-            boxShadow: "0px 4px 20.8px 0px rgba(188, 46, 255, 0.25)",
+            boxShadow: "0px 4px 60px 0px rgba(188, 46, 255, 0.25)",
           }}
         >
-          <p
-            className="text-[42px] font-semibold leading-[28px]"
+          <div
+            className="flex w-[789px] items-center justify-start gap-[66px] rounded-[16px] p-[40px] text-white"
             style={{
-              fontStyle: "italic",
+              background: "black",
             }}
           >
-            1
-          </p>
-          <p className="text-[24px] font-normal leading-[28px]">
-            <span className="font-semibold">Sign Up - </span>Create your
-            affiliate account in seconds
-          </p>
+            <p
+              className="bg-gradient-to-r from-[#0099FF] to-[#CC00FF] bg-clip-text p-1 text-[42px] font-semibold leading-[28px] text-transparent"
+              style={{
+                fontStyle: "italic",
+              }}
+            >
+              1
+            </p>
+            <p className="text-[24px] font-normal leading-[28px]">
+              <span className="font-semibold">Sign Up - </span>Create your
+              affiliate account in seconds
+            </p>
+          </div>
         </div>
-        <div
-          className="flex w-[789px] items-center justify-start gap-[66px] rounded-[16px] p-[40px] text-white"
-          style={{
-            borderRadius: "16px",
-            border: "1px solid rgba(255, 255, 255, 0.10)",
-            background: "rgba(116, 114, 138, 0.20)",
-          }}
-        >
-          <p
-            className="text-[42px] font-semibold leading-[28px]"
+        {["2", "3", "4"].map((number, index) => (
+          <div
+            key={index}
+            className="flex w-[789px] items-center justify-start gap-[66px] rounded-[16px] p-[40px] text-white transition-all duration-300 hover:bg-gradient-to-r hover:from-[#0099FF] hover:to-[#CC00FF] hover:shadow-[0px_4px_60px_0px_rgba(188,46,255,0.25)]"
             style={{
-              fontStyle: "italic",
+              borderRadius: "16px",
+              border: "1px solid rgba(255, 255, 255, 0.10)",
+              background: "rgba(116, 114, 138, 0.20)",
             }}
           >
-            2
-          </p>
-          <p className="text-[24px] font-normal leading-[28px]">
-            <span className="font-semibold">Share Your Link - </span>Get your
-            unique referral link and <br />
-            start sharing.
-          </p>
-        </div>
-        <div
-          className="flex w-[789px] items-center justify-start gap-[66px] rounded-[16px] p-[40px] text-white"
-          style={{
-            borderRadius: "16px",
-            border: "1px solid rgba(255, 255, 255, 0.10)",
-            background: "rgba(116, 114, 138, 0.20)",
-          }}
-        >
-          <p
-            className="text-[42px] font-semibold leading-[28px]"
-            style={{
-              fontStyle: "italic",
-            }}
-          >
-            3
-          </p>
-          <p className="text-[24px] font-normal leading-[28px]">
-            <span className="font-semibold">Earn 30% Commission - </span>Every
-            time someone <br /> subscribes using your link, you earn 30% of
-            their <br />
-            subscription.
-          </p>
-        </div>
-        <div
-          className="flex w-[789px] items-center justify-start gap-[66px] rounded-[16px] p-[40px] text-white"
-          style={{
-            borderRadius: "16px",
-            border: "1px solid rgba(255, 255, 255, 0.10)",
-            background: "rgba(116, 114, 138, 0.20)",
-          }}
-        >
-          <p
-            className="text-[42px] font-semibold leading-[28px]"
-            style={{
-              fontStyle: "italic",
-            }}
-          >
-            4
-          </p>
-          <p className="text-[24px] font-normal leading-[28px]">
-            <span className="font-semibold">Get Paid - </span>
-            Withdraw your earnings easily via your <br /> preferred payment
-            method.
-          </p>
-        </div>
+            <p
+              className="bg-gradient-to-r from-[#0099FF] to-[#CC00FF] bg-clip-text p-1 text-[42px] font-semibold leading-[28px] text-transparent"
+              style={{
+                fontStyle: "italic",
+              }}
+            >
+              {number}
+            </p>
+            <p className="text-[24px] font-normal leading-[28px]">
+              {number === "2" && (
+                <>
+                  <span className="font-semibold">Share Your Link - </span>Get
+                  your unique referral link and <br />
+                  start sharing.
+                </>
+              )}
+              {number === "3" && (
+                <>
+                  Earn{" "}
+                  <span className="bg-gradient-to-r from-[#0099FF] to-[#CC00FF] bg-clip-text text-transparent">
+                    30% Commission -{" "}
+                  </span>
+                  Every time someone <br /> subscribes using your link, you earn
+                  30% of their <br />
+                  subscription.
+                </>
+              )}
+              {number === "4" && (
+                <>
+                  <span className="font-semibold">Get Paid - </span>
+                  Withdraw your earnings easily via your <br /> preferred
+                  payment method.
+                </>
+              )}
+            </p>
+          </div>
+        ))}
       </div>
-    </div>
+    </Bounce>
   )
 }

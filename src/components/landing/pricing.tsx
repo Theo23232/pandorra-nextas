114f466ac6225354b85849e3768c1d142dfed27f
andritianaa <andritianasteve@gmail.com>
2025-02-10 15:27:37 +0300

    fix

--- src/components/landing/pricing.tsx
+++ src/components/landing/pricing.tsx
@@ -1,7 +1,12 @@
-import { Tab, TabContainer, TabPanel, Tabs } from '@/components/animated/animated-tabs';
-import Bounce from '@/components/animated/uibeats/bounce';
-import { Check } from '@/components/icons/check';
-import { Button } from '@/components/ui/button';
+import {
+  Tab,
+  TabContainer,
+  TabPanel,
+  Tabs,
+} from "@/components/animated/animated-tabs"
+import Bounce from "@/components/animated/uibeats/bounce"
+import { Check } from "@/components/icons/check"
+import { Button } from "@/components/ui/button"
 
 export const Pricing = () => {
   return (
@@ -25,11 +30,99 @@ export const Pricing = () => {
       <div className="relative flex min-h-[424px] w-full max-w-[1200px] items-center justify-center gap-9 pb-48 pt-8">
         <TabContainer className="relative flex flex-col items-center justify-center">
           <Tabs className="mb-32 text-white">
+            <Tab value={"small"}>Small</Tab>
             <Tab value={"monthly"}>Monthly</Tab>
             <Tab value={"annualy"} className="flex gap-2">
               Annualy <img src="/save-27.png" className="h-6" />
             </Tab>
           </Tabs>
+          <TabPanel
+            value={"small"}
+            className="relative flex justify-between gap-4"
+          >
+            <div className="flex min-h-[400px] w-[350px] shrink-0 flex-col items-start gap-[32px] rounded-[24px] border border-[rgba(255,255,255,0.10)] bg-[rgba(5,10,24,0.40)] p-[25px_32px]">
+              <div className="">
+                <p className="font-inter self-stretch text-[24px] font-bold leading-normal text-white">
+                  Free
+                </p>
+
+                <div className="mb-6 mt-8 flex flex-col gap-3">
+                  <div className="font-inter flex items-center text-[16px] font-normal leading-normal text-white">
+                    <Check className="mr-3" />
+                    <p className="gdt mr-1 font-extrabold"> 150</p>
+                    credits
+                  </div>
+
+                  <div className="font-inter flex items-center text-[16px] font-normal leading-normal text-white">
+                    <Check className="mr-3" /> Images:{" "}
+                    <p className="gdt mx-1 font-extrabold">5</p> AI-generated
+                    images
+                  </div>
+                  <div className="font-inter flex items-center text-[16px] font-normal leading-normal text-white">
+                    <Check className="mr-3" /> Text Generation:{" "}
+                    <p className="gdt mx-1 font-extrabold">20</p>
+                  </div>
+                  <div className="font-inter flex items-center text-[16px] font-normal leading-normal text-white">
+                    <Check className="mr-3" /> AI Voice:{" "}
+                    <p className="gdt mx-1 font-extrabold">5</p> minutes
+                  </div>
+                </div>
+
+                <div className="mb-8 flex items-center text-white">
+                  <p className="pt-2 text-[24px]"></p>
+                  <p className="text-[36px] font-bold"></p>
+                  <p className="ml-2 pt-2 font-light"></p>
+                </div>
+              </div>
+            </div>
+            <div className="flex min-h-[400px] w-[350px] items-center justify-center rounded-[24px] bg-gradient-to-br from-[#0099FF66] to-[#CC00FF77] p-[1px]">
+              <div className="flex h-full w-full shrink-0 flex-col items-start gap-[32px] rounded-[24px] bg-[#39517e77] p-[25px_32px]">
+                <div className="">
+                  <div className="flex items-center justify-between">
+                    <p className="font-inter self-stretch text-[24px] font-bold leading-normal text-white">
+                      Weekly
+                    </p>
+                  </div>
+                  <div className="mb-6 mt-8 flex flex-col gap-3">
+                    <div className="font-inter flex items-center text-[16px] font-normal leading-normal text-white">
+                      <Check className="mr-3" />
+                      <p className="gdt mr-1 font-extrabold"> 2500</p>
+                      credits
+                    </div>
+                    <div className="font-inter flex items-center text-[16px] font-normal leading-normal text-white">
+                      <Check className="mr-3" /> Short Video (5 sec):{" "}
+                      <p className="gdt mx-1 font-extrabold">62</p> videos
+                    </div>
+                    <div className="font-inter flex items-center text-[16px] font-normal leading-normal text-white">
+                      <Check className="mr-3" /> Medium Video (10 sec):{" "}
+                      <p className="gdt mx-1 font-extrabold">32</p> videos
+                    </div>
+                    <div className="font-inter flex items-center text-[16px] font-normal leading-normal text-white">
+                      <Check className="mr-3" /> Images:{" "}
+                      <p className="gdt mx-1 font-extrabold">125</p>{" "}
+                      AI-generated images
+                    </div>
+                    <div className="font-inter flex items-center text-[16px] font-normal leading-normal text-white">
+                      <Check className="mr-3" /> Text Generation:{" "}
+                      <p className="gdt mx-1 font-extrabold">500</p>
+                    </div>
+                    <div className="font-inter flex items-center text-[16px] font-normal leading-normal text-white">
+                      <Check className="mr-3" /> AI Voice:{" "}
+                      <p className="gdt mx-1 font-extrabold">125</p> minutes
+                    </div>
+                  </div>
+                  <div className="mb-8 flex items-center text-white">
+                    <p className="pt-2 text-[24px]">€</p>
+                    <p className="text-[36px] font-bold">32.99</p>
+                    <p className="ml-2 pt-2 font-light">/month</p>
+                  </div>
+                  <Button variant={"gradient"} className="w-full">
+                    <p className="">Choose</p>
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </TabPanel>
           <TabPanel
             value={"monthly"}
             className="relative flex justify-between gap-4"

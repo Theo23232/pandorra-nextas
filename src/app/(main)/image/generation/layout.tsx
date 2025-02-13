158b7601427729f3b0f5d1112c031c5191da492b
andritianaa <andritianasteve@gmail.com>
2025-02-13 21:23:18 +0300

    okok

--- src/app/(main)/image/generation/layout.tsx
+++ src/app/(main)/image/generation/layout.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import { redirect, useSearchParams } from "next/navigation"
-import { ReactNode } from "react"
+import { useSearchParams } from 'next/navigation';
+import { ReactNode } from 'react';
 
-import { useUser } from "@/hooks/use-user"
+import { useUser } from '@/hooks/use-user';
 
 export default function RouteLayout({ children }: { children: ReactNode }) {
   const { user } = useUser()
@@ -12,15 +12,15 @@ export default function RouteLayout({ children }: { children: ReactNode }) {
   const queryModelId = searchParams?.get("modelId")
   const queryImageSize = searchParams?.get("imageSize")
 
-  if (user) {
-    const presetStyle = user.imagePresetStylePreference
-    const modelId = user.imageModelIdPreference
-    const imageSize = user.imageSizePreference
-    if (!queryImageSize) {
-      return redirect(
-        `/image/generation?imageSize=${imageSize}&modelId=${modelId}&presetStyle=${presetStyle}`,
-      )
-    }
-  }
+  // if (user) {
+  //   const presetStyle = user.imagePresetStylePreference
+  //   const modelId = user.imageModelIdPreference
+  //   const imageSize = user.imageSizePreference
+  //   if (!queryImageSize) {
+  //     return redirect(
+  //       `/image/generation?imageSize=${imageSize}&modelId=${modelId}&presetStyle=${presetStyle}`,
+  //     )
+  //   }
+  // }
   return <>{children}</>
 }

3397eccc6eb67b5ef6c42632695aa088e88b016f
Miandry05 <miandryrakotovelo@gmail.com>
2025-02-17 16:56:09 +0300

    space to thousand

--- src/components/pandorra/jeton-counter.tsx
+++ src/components/pandorra/jeton-counter.tsx
@@ -1,20 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
 "use client"
-import Link from 'next/link';
-import { useTranslation } from 'react-i18next';
+import Link from "next/link"
+import { useTranslation } from "react-i18next"
 
-import { Tooltip } from '@/components/tremor/ui/tooltip';
-import { useUser } from '@/hooks/use-user';
-import { Plan } from '@prisma/client';
+import { Tooltip } from "@/components/tremor/ui/tooltip"
+import { useUser } from "@/hooks/use-user"
+import { Plan } from "@prisma/client"
 
 export default function JetonCounter() {
   const { t } = useTranslation()
   const { user } = useUser()
 
+  const formatJeton = (jeton: number) => {
+    return jeton.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")
+  }
+
   return (
     <div className="flex items-center justify-center gap-3">
       <Tooltip
-        content={`${t("You have")} ${user?.jeton} ${t(`tokens. Click to get more`)}`}
+        content={`${t("You have")} ${formatJeton(user?.jeton || 0)} ${t(`tokens. Click to get more`)}`}
       >
         <Link
           href={"/pricing"}
@@ -29,7 +33,7 @@ export default function JetonCounter() {
             id="tour3-step2"
             className="flex h-10 items-center justify-center gap-2 rounded-xl bg-accent px-4 font-bold hover:bg-muted"
           >
-            <span>{user?.jeton}</span>
+            <span>{formatJeton(user?.jeton || 0)}</span>
             <img src="/assets/token.png" className="h-6 w-auto" />
           </div>
         </Link>

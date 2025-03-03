2e9c601682af36d88796b7948dd16466ef58df0f
andritianaa <andritianasteve@gmail.com>
2025-03-03 12:58:44 +0300

    Remove loading component and implement a new loader animation in the video page

--- src/app/(web)/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <p className="text-4xl">Loading...</p>
-    </div>
-  )
-}
--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -79,7 +79,53 @@ export default function RoutePage() {
   // Affichage de l'écran de chargement avec la vidéo
   if (!videoEnded) {
     return (
-      <div className="flex h-screen w-full flex-col items-center justify-center bg-[#010101]">
+      <div className="flex h-screen w-full flex-col items-center justify-center bg-[#010101] text-white">
+        {!videoLoaded && (
+          <div className="fixed inset-0 flex items-center justify-center">
+            <div className="loader">
+              <svg viewBox="0 0 100 100" height="100px" width="100px">
+                <defs>
+                  <linearGradient
+                    y2="0%"
+                    x2="100%"
+                    y1="0%"
+                    x1="0%"
+                    id="gradient1"
+                  >
+                    <stop stop-color="#4f8ef7" offset="0%"></stop>
+                    <stop stop-color="#a663cc" offset="50%"></stop>
+                    <stop stop-color="#f74f6f" offset="100%"></stop>
+                  </linearGradient>
+                  <linearGradient
+                    y2="0%"
+                    x2="100%"
+                    y1="0%"
+                    x1="0%"
+                    id="gradient2"
+                  >
+                    <stop stop-color="#f7b34f" offset="0%"></stop>
+                    <stop stop-color="#5ef7a5" offset="50%"></stop>
+                    <stop stop-color="#4f8ef7" offset="100%"></stop>
+                  </linearGradient>
+                </defs>
+                <circle
+                  stroke="url(#gradient1)"
+                  r="40"
+                  cy="50"
+                  cx="50"
+                  className="loader-circle circle-1"
+                ></circle>
+                <circle
+                  stroke="url(#gradient2)"
+                  r="30"
+                  cy="50"
+                  cx="50"
+                  className="loader-circle circle-2"
+                ></circle>
+              </svg>
+            </div>
+          </div>
+        )}
         <div
           className={`h-full w-full transition-opacity duration-1000 ${fadeOut ? "opacity-0" : "opacity-100"}`}
         >
@@ -90,9 +136,8 @@ export default function RoutePage() {
             playsInline
             autoPlay
             muted
-            onEnded={() => {
-              setVideoEnded(true)
-            }}
+            onLoadedData={() => setVideoLoaded(true)}
+            onEnded={() => setVideoEnded(true)}
           />
         </div>
       </div>
